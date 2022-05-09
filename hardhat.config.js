33167abb0f71815a3406a41e3b47e84802252958
Yash Shah <yashshh123@gmail.com>
2022-05-10 01:47:19 +0530

    NFT contract deployment first commit
    NFT contract deployment first commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+console.log(ALCHEMY_API_KEY_URL)
+console.log(RINKEBY_PRIVATE_KEY)
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
